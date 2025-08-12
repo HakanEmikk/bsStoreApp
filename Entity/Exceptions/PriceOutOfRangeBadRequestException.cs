0b5218d0af8b42df94964a289c17b59c8a283e78
hakanemik51 <hkn.emk51@gmail.com>
2025-08-12 16:51:26 +0300

    filter operation added

---
+++ Entity/Exceptions/PriceOutOfRangeBadRequestException.cs
@@ -0,0 +1,14 @@
+﻿namespace Entity.Exceptions
+{
+    public abstract partial class BadRequestException
+    {
+        public class PriceOutOfRangeBadRequestException : BadRequestException
+        {
+            public PriceOutOfRangeBadRequestException() 
+                : base("Maximun price should be less than 1000 and greater than 10.")
+            {
+                
+            }
+        }
+    }
+}
